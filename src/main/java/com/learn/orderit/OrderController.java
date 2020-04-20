9dc1829d10ee59957b9e0e988e6cc20635244630
Sneha Madle <smmadle235@gmail.com>
2020-04-20 21:15:58 +0530

    Task:1 Expose a GET API

---
+++ src/main/java/com/learn/orderit/OrderController.java
@@ -0,0 +1,33 @@
+package com.learn.orderit;
+
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Controller
+@Produces(MediaType.APPLICATION_JSON)
+@RequestMapping("/order")
+@Api(description = "Order creation API for orderit",
+        produces = MediaType.APPLICATION_JSON)
+public class OrderController {
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GET
+    @RequestMapping("/create")
+    @ResponseBody
+    public String createOrder(){
+        String result = orderService.createOrder();
+        return result;
+    }
+}
---
+++ src/test/java/com/learn/orderit/OrderitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.orderit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
